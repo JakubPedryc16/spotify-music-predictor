f3e40be73ad0f3d558aec8584e0a006aaa620bd0
Jakub Pedryc <jakubpedryc@gmail.com>
2025-10-20 17:32:03 +0200

    Fix: main.py properly deletes 10% od data

--- main.py
+++ main.py
@@ -2,15 +2,23 @@ import pandas as pd
 import numpy as np
 
 df = pd.read_csv("dataset.csv")
+df = df.rename(columns={'Unnamed: 0': 'id'})
+
 print(df.head())
 print(df.info())
 
 print(df.isnull().sum())
 print(df[df.isnull().any(axis=1)])
 
-if df.isnull().sum().sum() < 10:
-    df_mask = pd.DataFrame(np.random.rand(*df.shape) < 0.1, columns=df.columns)
-    df = df.mask(df_mask)
+columns_to_mask = df.columns.difference(['id'])
 
+
+df_mask = pd.DataFrame(np.random.rand(
+    df.shape[0],
+    len(columns_to_mask)) < 0.1,
+    columns=columns_to_mask
+)
+
+df[columns_to_mask] = df[columns_to_mask].mask(df_mask)
 
 df.to_csv("dataset_with_missing.csv", index=False)
